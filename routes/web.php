c022c6a8df8b7bf73a55bd60d89b40f61e7e8141
Nur Melisa <nur0melisaoyee@gmail.com>
2024-10-02 12:19:19 +0700

    praktikum 5 - pertemuan 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriControllerr;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 
@@ -18,3 +19,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriControllerr::class, 'index']);
