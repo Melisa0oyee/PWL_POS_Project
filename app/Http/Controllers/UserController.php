28b613ca3abf0508a3bad0f2377d5c55f8524de0
Nur Melisa <nur0melisaoyee@gmail.com>
2024-10-08 00:31:43 +0700

    praktikum 1 - pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,12 +23,28 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data); // tambahkan data ke tabel m_user
         
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
     
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama'     => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama'     => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view ('user', ['data' => $user]);
