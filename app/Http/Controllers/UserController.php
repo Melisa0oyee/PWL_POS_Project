501703b2f35d1dbf70243356a6a5f97af228359c
Nur Melisa <nur0melisaoyee@gmail.com>
2024-10-11 21:33:42 +0700

    praktikum 2.6 - pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
+   // public function index()
+    //{
         // // coba akses model UserModel
-        // $user = UserModel::all(); //ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
+        //$user = UserModel::all(); //ambil semua data dari tabel m_user
+        //return view('user', ['data' => $user]);
 
         // tambah data user dengan Eqloquent Model
         // $data = [
@@ -157,21 +157,70 @@ class UserController extends Controller
 
         
         // LANGKAH 2 PRAK 2.5 - PERTEMUAN 4
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager 11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user->username = 'manager12';
-        $user->save();
+        // $user = UserModel::create(
+        //     [
+        //         'username' => 'manager11',
+        //         'nama' => 'Manager 11',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->username = 'manager12';
+        // $user->save();
 
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd ($user->wasChanged(['nama', 'username'])); // true
+        // $user->wasChanged(); // true
+        // $user->wasChanged('username'); // true
+        // $user->wasChanged(['username', 'level_id']); // true
+        // $user->wasChanged('nama'); // false
+        // dd ($user->wasChanged(['nama', 'username'])); // true 
+    //}
+
+     // LANGKAH 6 PRAKTIKUM 2.6 PERTEMUAN 4
+     public function index()
+     {
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);  
+     }
+     public function tambah()
+     {
+        return view('user_tambah');
+     }
+     public function tambah_simpan(Request $request)
+     {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+     }
+     public function ubah($id) 
+     {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+     }
+
+     public function ubah_simpan($id, Request $request)
+     {
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+    
+         // Pastikan password di-hash dengan benar dan diambil dari request.
+        $user->password = Hash::make($request->password); 
+        $user->level_id = $request->level_id;
+
+        $user->save(); // Simpan perubahan ke database
+
+        return redirect('/user');
     }
+
+    public function hapus($id)
+    {
+      $user = UserModel::find($id);
+      $user->delete();
+      return redirect('/user');
+    }
+
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,23 +5,27 @@
     </head>
     <body>
         <h1>Data User</h1>
+        <a href="http://PWL_POS_Project.test/user/tambah">Tambah User</a>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th> 
-                
+                <th>Aksi</th>
             </tr>
-            {{-- @foreach ($data as $d) --}}
+            @foreach ($data as $d)
             <tr>
-                <td>{{ $data ->user_id }}</td>
-                <td>{{ $data ->username }}</td>
-                <td>{{ $data ->nama }}</td>
-                <td>{{ $data ->level_id }}</td>
-                
+                <td>{{ $d ->user_id }}</td>
+                <td>{{ $d ->username }}</td>
+                <td>{{ $d ->nama }}</td>
+                <td>{{ $d ->level_id }}</td>
+                <td>
+                    <a href="http://PWL_POS_Project.test/user/ubah/{{ $d->user_id}}">Ubah</a> | 
+                    <a href="http://PWL_POS_Project.test/user/hapus/{{ $d->user_id}}">Hapus</a>
+                </td>
             </tr>
-            {{-- @endforeach --}}
+            @endforeach
         </table>
     </body>
 </html>
