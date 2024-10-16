d872761174bed4936612a955626160e6a34ced7e
Nur Melisa <nur0melisaoyee@gmail.com>
2024-10-16 11:57:18 +0700

    praktikum 2 - pertemuan 8

--- routes/web.php
+++ routes/web.php
@@ -117,6 +117,7 @@ Route::middleware(['authorize:ADM,MNG'])->group (function (){
     Route::delete('/barang/{id}', [BarangController::class, 'destroy']);     // menghapus data user
     Route::get('/barang/import', [BarangController::class, 'import']);       // ajax form upload excel
     Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+    Route::get('/barang/export_excel', [BarangController::class, 'export_excel']); // export excel
 });
 
 });
