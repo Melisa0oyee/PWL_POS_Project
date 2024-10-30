199753f539dc40e410c6fb1009679c6c261226ab
Nur Melisa <nur0melisaoyee@gmail.com>
2024-10-30 15:15:07 +0700

    TUGAS - pertemuan 10

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,8 +9,38 @@ use App\Models\UserModel; // Pastikan Anda menggunakan model yang benar
 
 class UserController extends Controller
 {
+    public function index ()
+    {
+        return UserModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $user = UserModel::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    public function show(UserModel $user)
+    {
+        return UserModel::find($user);
+    }
+
+    public function update(Request $request, UserModel $user)
+    {
+        $user->update($request->all());
+        return UserModel::find($user);
+    }
+    public function destroy(UserModel $user)
+    {
+        $user->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data user terhapus',
+        ]);
+    }
     public function getUser( Request $request )
     {
+        
         // Mendapatkan pengguna yang sedang login
         $user = auth()->guard('api')->user();
 
